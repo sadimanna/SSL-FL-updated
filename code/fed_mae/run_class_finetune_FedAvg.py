4927da64d1bac199c63451ea33bef634a62ed3b5
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-10-18 12:00:33 +0800

    Update run_class_finetune_FedAvg.py

--- code/fed_mae/run_class_finetune_FedAvg.py
+++ code/fed_mae/run_class_finetune_FedAvg.py
@@ -17,7 +17,7 @@ import torch.backends.cudnn as cudnn
 from torch.utils.tensorboard import SummaryWriter
 
 import timm
-assert timm.__version__ == "0.3.2" # version check
+# assert timm.__version__ == "0.3.2" # version check
 from copy import deepcopy
 
 import os
@@ -106,8 +106,8 @@ def get_args():
     # * Finetuning params
     parser.add_argument('--finetune', default='',
                         help='finetune from checkpoint')
-    parser.add_argument('--global_pool', action='store_true')
-    parser.set_defaults(global_pool=True)
+    parser.add_argument('--global_pool', help='token, avg, avgmax, max')
+    parser.set_defaults(global_pool='avg')
     parser.add_argument('--cls_token', action='store_false', dest='global_pool',
                         help='Use class token instead of global pool for classification')
 
@@ -143,7 +143,7 @@ def get_args():
     # distributed training parameters
     parser.add_argument('--world_size', default=1, type=int,
                         help='number of distributed processes')
-    parser.add_argument('--local_rank', default=-1, type=int)
+    # parser.add_argument('--local_rank', default=-1, type=int)
     parser.add_argument('--sync_bn', default=False, action='store_true')
     parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://',
@@ -402,6 +402,7 @@ def main(args, model):
 
 if __name__ == '__main__':
     args = get_args()
+    args.local_rank = int(os.environ["LOCAL_RANK"])
 
     if args.output_dir:
         Path(args.output_dir).mkdir(parents=True, exist_ok=True)
