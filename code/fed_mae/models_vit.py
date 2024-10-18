1a68240c44933b5a4cb56b88fdee92a7bbc9095b
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-10-18 11:55:40 +0800

    Update models_vit.py

--- code/fed_mae/models_vit.py
+++ code/fed_mae/models_vit.py
@@ -20,37 +20,42 @@ import timm.models.vision_transformer
 class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """ Vision Transformer with support for global average pooling
     """
-    def __init__(self, global_pool=False, **kwargs):
+    def __init__(self, global_pool='token', **kwargs):
         super(VisionTransformer, self).__init__(**kwargs)
 
         self.global_pool = global_pool
-        if self.global_pool:
-            norm_layer = kwargs['norm_layer']
-            embed_dim = kwargs['embed_dim']
-            self.fc_norm = norm_layer(embed_dim)
+        # if self.global_pool:
+        #     norm_layer = kwargs['norm_layer']
+        #     embed_dim = kwargs['embed_dim']
+        #     self.fc_norm = norm_layer(embed_dim)
 
-            del self.norm  # remove the original norm
+        #     del self.norm  # remove the original norm
 
-    def forward_features(self, x):
-        B = x.shape[0]
-        x = self.patch_embed(x)
+    # def forward_features(self, x):
+    #     B = x.shape[0]
+    #     x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
-        x = torch.cat((cls_tokens, x), dim=1)
-        x = x + self.pos_embed
-        x = self.pos_drop(x)
+    #     cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+    #     x = torch.cat((cls_tokens, x), dim=1)
+    #     x = x + self.pos_embed
+    #     x = self.pos_drop(x)
 
-        for blk in self.blocks:
-            x = blk(x)
+    #     for blk in self.blocks:
+    #         x = blk(x)
 
-        if self.global_pool:
-            x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
-            outcome = self.fc_norm(x)
-        else:
-            x = self.norm(x)
-            outcome = x[:, 0]
+    #     final_norm = True
+    #     if self.global_pool:
+    #         use_fc_norm True
+    #         norm is Identity
+    #         x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
+    #         outcome = self.fc_norm(x)
+    #     else:
+    #         use_fc_norm False
+    #         x = self.norm(x)
+    #         outcome = x[:, 0] # token pooling
+    #         fc_norm is Identity
 
-        return outcome
+    #     return outcome
 
 
 def vit_base_patch16(**kwargs):
